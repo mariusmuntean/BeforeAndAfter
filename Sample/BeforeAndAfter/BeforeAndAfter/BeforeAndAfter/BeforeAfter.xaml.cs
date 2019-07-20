ad334ff211e113b9424a577b0caf5fe2cd6ef2a6
Marius <marius.muntean@live.com>
2019-07-20 16:56:31 +0200

    Little improvements

--- Sample/BeforeAndAfter/BeforeAndAfter/BeforeAndAfter/BeforeAfter.xaml.cs
+++ Sample/BeforeAndAfter/BeforeAndAfter/BeforeAndAfter/BeforeAfter.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,6 +7,9 @@ namespace BeforeAndAfter
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BeforeAfter : ContentView
     {
+        private const string Offset = "Offset";
+        private const string NegativeOffset = "NegativeOffset";
+
         public BeforeAfter()
         {
             InitializeComponent();
@@ -16,23 +19,25 @@ namespace BeforeAndAfter
             nameof(BeforeView),
             typeof(View),
             typeof(BeforeAfter),
-            new BoxView { BackgroundColor = Color.FromHex("#FFFD700") },
+            new BoxView {BackgroundColor = Color.FromHex("#FFFD700")},
             propertyChanged: OnBeforeViewChanged
         );
 
         private static void OnBeforeViewChanged(BindableObject bindable, object oldvalue, object newvalue)
         {
-            if (bindable is BeforeAfter beforeAfter && newvalue is View newView)
+            if (bindable is BeforeAfter beforeAfter)
             {
                 // Remove any previous BeforeView
-                var childrenToBeRemoved = beforeAfter.LayoutRoot.Children.Where(view => view != beforeAfter.AfterViewParentLayout).ToList();
-                if (childrenToBeRemoved.Any())
+                if (oldvalue is View oldView && beforeAfter.LayoutRoot.Children.Contains(oldView))
                 {
-                    childrenToBeRemoved.ForEach(view => beforeAfter.LayoutRoot.Children.Remove(view));
+                    beforeAfter.LayoutRoot.Children.Remove(oldView);
                 }
 
-                // Add the new child to the before view
-                beforeAfter.LayoutRoot.Children.Insert(0, newView);
+                // Add the new BeforeView
+                if (newvalue is View newView)
+                {
+                    beforeAfter.LayoutRoot.Children.Insert(0, newView);
+                }
             }
         }
 
@@ -46,21 +51,25 @@ namespace BeforeAndAfter
             nameof(AfterView),
             typeof(View),
             typeof(BeforeAfter),
-            new BoxView { BackgroundColor = Color.FromHex("#FFFD700") },
+            new BoxView {BackgroundColor = Color.FromHex("#FFFD700")},
             propertyChanged: OnAfterViewChanged
         );
 
         private static void OnAfterViewChanged(BindableObject bindable, object oldvalue, object newvalue)
         {
-            if (bindable is BeforeAfter beforeAfter && newvalue is View newView)
+            if (bindable is BeforeAfter beforeAfter)
             {
                 // Remove any previous AfterView
                 beforeAfter.AfterViewParentLayout.Children.Clear();
 
-                // Add the new child to the after view
-                beforeAfter.AfterViewParentLayout.Children.Add(newView);
+                // Add the new AfterView
+                if (newvalue is View newView)
+                {
+                    beforeAfter.AfterViewParentLayout.Children.Add(newView);
+                    //newView.TranslationX = (double) beforeAfter.Resources["NegativeOffset"];
 
-                newView.TranslationX = (double) beforeAfter.Resources["NegativeOffset"];
+                    newView.SetDynamicResource(TranslationXProperty, NegativeOffset);
+                }
             }
         }
 
@@ -68,6 +77,39 @@ namespace BeforeAndAfter
         {
             get => GetValue(AfterViewProperty) as View;
             set => SetValue(AfterViewProperty, value);
+        }
+
+
+        double previousTranslateX = 0.0d;
+
+        private void PanGestureRecognizer_OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    previousTranslateX = e.TotalX;
+                    break;
+
+                case GestureStatus.Running:
+                    AddOffsetDelta(e.TotalX - previousTranslateX);
+                    previousTranslateX = e.TotalX;
+                    break;
+
+                case GestureStatus.Completed:
+                    break;
+
+                case GestureStatus.Canceled:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void AddOffsetDelta(double offsetDelta)
+        {
+            this.Resources[Offset] = ((double) this.Resources[Offset]) + offsetDelta;
+            this.Resources[NegativeOffset] = -((double) this.Resources[Offset]);
         }
     }
 }
